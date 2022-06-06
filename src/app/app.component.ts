bafc90740246d428c3d2e9e1c5109b8be9e67477
fruizfleetani <fernandoruizfleetani@gmail.com>
2022-06-06 10:19:16 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // Variables
   title = 'pruebaAngular';
+  variable = true
+  casas = [{nombre: 'Casa en Atarfe', precio: 1200, img: "https://assets-global.website-files.com/5f4f67c5950db17954dd4f52/5f5b7ee442f1e5b9fee1c117_hacerse-una-casa.jpeg"},
+  {nombre: 'Piso', precio: 1000, img: "https://assets-global.website-files.com/5f4f67c5950db17954dd4f52/5f5b7ee442f1e5b9fee1c117_hacerse-una-casa.jpeg"},
+  {nombre: 'Casa en Pinos', precio: 800, img: "https://assets-global.website-files.com/5f4f67c5950db17954dd4f52/5f5b7ee442f1e5b9fee1c117_hacerse-una-casa.jpeg"}];
+  listado= false
+
+
+  // Métodos
+  obtenerListado(){
+    this.listado = !this.listado
+   
+  }
+  detalle(){
+    console.log("Funciona maquina")
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CasaComponent } from './casa/casa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CasaComponent
   ],
   imports: [
     BrowserModule
